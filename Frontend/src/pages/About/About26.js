7115e86812944b5d2826a83036f3407c16aa76bf
Preeth Bera <preethbera@gmail.com>
2025-09-04 23:44:49 +0530

    about us section

--- Frontend/src/pages/About/About26.js
+++ Frontend/src/pages/About/About26.js
@@ -1,14 +1,66 @@
+import { useState, useEffect, useRef } from "react";
 import styles from "./about26.module.css";
 
 function About() {
+  // Store multiple images here
+  const images = [
+    "https://i.postimg.cc/Y9yk6M7k/image.png",
+    "https://i.postimg.cc/KcJ3WMGv/image.png",
+    "https://i.postimg.cc/XXSRLvzM/image.png",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const intervalRef = useRef(null);
+
+  // Preload images once
+  useEffect(() => {
+    images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, [images]);
+
+  // Start auto change
+  const startSlideshow = () => {
+    intervalRef.current = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % images.length);
+    }, 2000); // 3s
+  };
+
+  const stopSlideshow = () => {
+    if (intervalRef.current) clearInterval(intervalRef.current);
+  };
+
+  useEffect(() => {
+    startSlideshow();
+    return () => stopSlideshow();
+  }, [images.length]);
+
   return (
     <div className={styles.container}>
-      <div className={styles.imageBox}>
+      <div
+        className={styles.imageBox}
+        onMouseEnter={stopSlideshow}
+        onMouseLeave={startSlideshow}
+      >
         <img
-          src="https://www.iitkgpfoundation.org/images/vault/40.jpg"
+          src={images[currentIndex]}
           alt="Kshitij IIT Kharagpur"
-          className={styles.image}
+          className={`${styles.image} ${styles.show}`}
         />
+
+        {/* Dots Navigation */}
+        <div className={styles.dots}>
+          {images.map((_, index) => (
+            <span
+              key={index}
+              className={`${styles.dot} ${
+                currentIndex === index ? styles.activeDot : ""
+              }`}
+              onClick={() => setCurrentIndex(index)}
+            />
+          ))}
+        </div>
       </div>
 
       <div className={styles.box}>
@@ -20,9 +72,8 @@ function About() {
           Ambassador Program brings an opportunity for all the students across
           the country to be a part of team Kshitij, IIT Kharagpur. The Campus
           Ambassadors act as the backbone of the fest by bridging between
-          Kshitij and the students of their respective colleges.{" "}
+          Kshitij and the students of their respective colleges.
         </p>
-        <br />
         <p className={styles.para}>
           The Campus Ambassador Program is one of the biggest platforms for
           students to engage in serious networking and gain knowledge of
