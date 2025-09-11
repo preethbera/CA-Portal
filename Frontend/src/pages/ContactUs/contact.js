7d04fe0b0521bbfd954974e23b836184f2c0c8a9
Akshat-jwr <akshat2306jwr3@gmail.com>
2025-09-12 00:20:38 +0530

    fixes and updates

--- Frontend/src/pages/ContactUs/contact.js
+++ Frontend/src/pages/ContactUs/contact.js
@@ -9,9 +9,10 @@ function Contact() {
     <div className={styles.covv}>
       <h2 className={styles.heading}>CONTACT US</h2>
       <div className={styles.main}>
-          <Card img={shuvam} name="Avinash Ghuge" job="Publicity and Media Head, Megashows Coordinator, Overseas Coordinator" num="7058598614" email="ghuge.avinash@ktj.in" fb = "https://www.facebook.com/profile.php?id=61552726551110&mibextid=ZbWKwL" li="https://www.linkedin.com/in/avinash-ghuge-7b5224295?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"/>
+          <Card img="https://i.postimg.cc/ZRDBJjxM/image.png" name="Avinash Ghuge" job="Publicity and Media Head, Megashows Coordinator, Overseas Coordinator" num="7058598614" email="ghuge.avinash@ktj.in" fb = "https://www.facebook.com/profile.php?id=61552726551110&mibextid=ZbWKwL" li="https://www.linkedin.com/in/avinash-ghuge-7b5224295?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"/>
           {/* <Card img={Kajal} name="Kajal Singh" job="Publicity Head, Design Coordinator, Auto Expo Coordinator" num="8433450232" email="singh.kajal@ktj.in" fb = "https://www.facebook.com/profile.php?id=100016684572655" li="https://www.linkedin.com/in/kajal-singh-164242213"/> */}
-          <Card img={saket} name="Kshitij Kamble" job="Publicity and Media Head, Design Coordinator, Exhibition and Auto-Expo Coordinator" num="8956508116" email="kshitij.kamble@ktj.in" fb = "https://www.facebook.com/profile.php?id=100095652065104&mibextid=LQQJ4d" li="https://www.linkedin.com/in/kshitij-kamble-75b079287/"/>
+          <Card img="https://i.postimg.cc/sDT8shkZ/image.png" name="Kshitij Kamble" job="Publicity and Media Head, Design Coordinator, Exhibition and Auto-Expo Coordinator" num="8956508116" email="kshitij.kamble@ktj.in" fb = "https://www.facebook.com/profile.php?id=100095652065104&mibextid=LQQJ4d" li="https://www.linkedin.com/in/kshitij-kamble-75b079287/"/>
+          <Card img="https://i.postimg.cc/hv593cYh/image.png" name="Anirudh Agrawal" job="Publicity and Media Head, Design Coordinator, Social Initiative Coordinator" num="9335047408" email="agrawal.anirudh@ktj.in" fb = "https://www.facebook.com/share/1DECx8kRcK/?mibextid=wwXIfr" li="https://www.linkedin.com/in/anirudh-agrawal-74225a188?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"/>
       </div>
     </div>
   );
--- Frontend/src/pages/companyIncentives/CompanyIncentives.js
+++ Frontend/src/pages/companyIncentives/CompanyIncentives.js
@@ -8,26 +8,32 @@ import Card from "./ScrapperCard";
 // import { Divide } from 'lucide-react';
 function CompanyIncentives() {
   return (
-    <div style={{ minHeight: "100vh", contentAlign: "center" }}>
+    <div style={{ minHeight: "50vh", contentAlign: "center" }}>
       <h2 className={styles.heading}>Gift From Partners</h2>
       <div className={styles.companyIncentivesGrid}>
         <Card
-          img={abhibus}
-          title={"Red Bus"}
-          subtitle={"Travel Partner"}
-          desc={"Get amazing discounts on bus bookings."}
+          img={"https://i.ibb.co/ZRLXzmKx/Screenshot-2025-09-12-at-12-10-00-AM.png"}
+          title={"Skill Ladder"}
+          subtitle={"EdTech Platform"}
+          desc={"The top 50 Campus Ambassadors will get free upskilling courses that are available on their website"}
         />
         <Card
-          img={elm}
-          title={"ELM"}
+          img={"https://i.ibb.co/nVDHkyG/Screenshot-2025-09-12-at-12-03-56-AM.png"}
+          title={"IALM"}
           subtitle={"E-Learning Platform"}
-          desc={"Access a wide range of online courses."}
+          desc={"The top 50 Campus Ambassadors will get a 50% discount on all Law courses available on their website"}
         />
         <Card
-          img={stackedge}
-          title={"StackEdge"}
-          subtitle={"Cloud Solutions"}
-          desc={"Optimize your cloud infrastructure with us."}
+          img={"https://i.ibb.co/4gJ5TBj0/Screenshot-2025-09-12-at-12-06-28-AM.png"}
+          title={"Experiment Labs"}
+          subtitle={"EdTech Platform"}
+          desc={"Campus Ambassadors will get upto 100% discounts on their mentorship programs based on their performance"}
+        />
+        <Card
+          img={"https://i.ibb.co/4nTRkhWc/Screenshot-2025-09-12-at-12-13-29-AM.png"}
+          title={"Triple Solutions"}
+          subtitle={"E-Learning Platform"}
+          desc={"Campus Ambassadors will get upto 100% discounts on their mentorship programs based on their performance"}
         />
       </div>
     </div>
